baed19ee68685308b7564a671037a1a12b4b1999
王品朝 <wangqihuan@viewhigh.com>
2019-08-08 07:35:42 +0800

    yilai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { coreModule } from './core/core.module';
+
+// material module
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    coreModule // 引入全局依赖
+    coreModule, // 引入全局依赖
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
