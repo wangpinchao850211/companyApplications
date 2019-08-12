766cfade00c42db2dff849d41127f309619f5718
王品朝 <wangqihuan@viewhigh.com>
2019-08-12 21:47:53 +0800

    sidenav 使用mat-list

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import {loadSvgResources} from '../utils/svg.util';
 @NgModule({
   declarations: [
     HeaderComponent,
@@ -23,11 +25,16 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ]
 })
 export class coreModule {
-  constructor (@Optional() @SkipSelf() parent: coreModule) {
+  constructor (
+    @Optional() @SkipSelf() parent: coreModule,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
     // @SkipSelf()告知去父级去寻找是否有此类已经加载
     // @Optional()首次加载时，一定没有这个类，optional指定为可选参数
     if (parent) {
       throw new Error('模块已经存在，不能再次加载!');
     }
+    loadSvgResources(iconRegistry, sanitizer);
   }
 }
