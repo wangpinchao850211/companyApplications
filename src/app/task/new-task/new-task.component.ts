0e6eff63a5f1a994b2468e81450024145118e579
Wang <qihuan.wang@accenture.com>
2019-09-18 19:00:23 +0800

    chips

--- src/app/task/new-task/new-task.component.ts
+++ src/app/task/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit, Inject } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
@@ -8,6 +9,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 })
 export class NewTaskComponent implements OnInit {
 
+  form: FormGroup;
   dialogTitle: string;
   priorities: { label: string; value: number }[] = [
     {
@@ -24,12 +26,55 @@ export class NewTaskComponent implements OnInit {
     },
   ];
   constructor(
+    private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<NewTaskComponent>
   ) { }
 
   ngOnInit() {
     this.dialogTitle = this.data.title;
     console.log(JSON.stringify(this.data.title));
+    // if (!this.data.task) {
+    //   this.form = this.fb.group({
+    //     desc: ['', Validators.compose([Validators.required, Validators.maxLength(30)])],
+    //     priority: [3],
+    //     dueDate: [],
+    //     reminder: [],
+    //     owner: [[this.data.owner]],
+    //     followers: [[]],
+    //     remark: ['',  Validators.maxLength(40)]
+    //   });
+    //   this.dialogTitle = '创建任务：';
+    //   this.delInvisible = true;
+    // } else {
+    //   this.form = this.fb.group({
+    //     desc: [this.data.task.desc, Validators.compose([Validators.required, Validators.maxLength(20)])],
+    //     priority: [this.data.task.priority],
+    //     dueDate: [this.data.task.dueDate],
+    //     reminder: [this.data.task.reminder],
+    //     owner: [this.data.task.owner ? [{name: this.data.task.owner.name, value: this.data.task.owner.id}] : []],
+    //     followers: [this.data.task.participants ? [...this.data.task.participants] : []],
+    //     remark: [this.data.task.remark, Validators.maxLength(40)]
+    //   });
+    //   this.dialogTitle = '修改任务：';
+    //   this.delInvisible = false;
+    // }
+  }
+
+  onSubmit({value, valid}, ev: Event) {
+    ev.preventDefault();
+    if (!valid) {
+      return;
+    }
+    // this.dialogRef.close({type: 'addOrUpdate', task: {
+    //   desc: value.desc,
+    //   participantIds: value.followers.map(u => u.id),
+    //   ownerId: value.owner.length > 0 ? value.owner[0].id : null,
+    //   dueDate: value.dueDate,
+    //   reminder: value.reminder,
+    //   priority: value.priority,
+    //   remark: value.remark
+    // }});
   }
 
 }
